5417784733be666bdf9da4cfed81a1b751607075
Jessica Morrison <jnicomorr@gmail.com>
2022-05-23 21:34:47 -0500

    Added books on the dom

--- src/api/bookData.js
+++ src/api/bookData.js
@@ -1,11 +1,15 @@
-// import axios from 'axios';
-// import firebaseConfig from './apiKeys';
+import axios from 'axios';
+import firebaseConfig from './apiKeys';
 // API CALLS FOR BOOKS
 
-// const dbUrl = firebaseConfig.databaseURL;
+const dbUrl = firebaseConfig.databaseURL;
 
 // TODO: GET BOOKS
-const getBooks = () => {};
+const getBooks = () => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/books.json`)
+    .then((response) => resolve(Object.values(response.data)))
+    .catch((error) => reject(error));
+});
 
 // TODO: DELETE BOOK
 const deleteBook = () => {};
